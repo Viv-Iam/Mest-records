b7264991e5cf7d18717122f5ae777af7008fe627
Vivi-Iam <vivian.opondoh@gmail.com>
2018-09-16 00:06:44 +0300

    Add method for fetching data from DB

--- records/add.js
+++ records/add.js
@@ -1,11 +1,10 @@
 import { Template } from 'meteor/templating';
+import './api/records.js'
 
 import './add.html';
 
 Template.body.helpers({
-    tasks: [
-      { text: 'This is task 1' },
-      { text: 'This is task 2' },
-      { text: 'This is task 3' },
-    ],
+    records() {
+      return Records.find({})
+    }
   });
