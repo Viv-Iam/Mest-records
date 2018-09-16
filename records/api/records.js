61b05a7923e3624f059d42ea31883f7a2c1d2bcd
Vivi-Iam <vivian.opondoh@gmail.com>
2018-09-16 09:40:16 +0300

    Add meteor method for inserting data into collection

--- records/api/records.js
+++ records/api/records.js
@@ -1,3 +1,21 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Records = new Mongo.Collection('records');
+
+Meteor.methods ({
+    'eit.insert' (eitdata) {
+        check(eitdata, String);
+
+        //Ensure user is logged in
+        if(!Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Records.insert ({
+            eitdata,
+            createdAt: new Date(),
+        })
+    }
+})
