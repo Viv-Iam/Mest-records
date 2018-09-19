52dc554eff4aebbac05e93a58c5052e04d935804
Vivi-Iam <vivian.opondoh@gmail.com>
2018-09-19 04:04:13 +0300

    Move multiple methods to add.js
    *Remove unused code
    *Move remove and toggle methods

--- imports/ui/add.js
+++ imports/ui/add.js
@@ -5,6 +5,8 @@ import { Records } from '../api/records.js';
 import './add.html';
 import './list.js';
 
+var selected = [];
+
 Template.body.helpers({
     records() {
       return Records.find({})
@@ -15,24 +17,41 @@ Template.body.helpers({
     'submit .eitdata' (event) {
       // Prevent default browser form submit
       event.preventDefault();
-   
-      // Get value from form element
       const target = event.target;
-      const firstName = target.firstName.value;
-      const lastName = target.lastName.value;
-      const gender = target.gender.value;
-      const dob = target.dob.value;
-      // Insert a record into the collection
-      Meteor.call('records.insert', firstName, lastName, gender, dob);
+      
+      var id = target.id.value;
+      console.log('ID', id)
 
-      // Clear form
+      var eitrecord = {firstName: target.firstName.value , 
+                        lastName: target.lastName.value,
+                        gender:target.gender.value,
+                        dob:target.dob.value};
+      if(id === ''){
+      Meteor.call('records.insert', eitrecord); 
+    } else {
+      Meteor.call('records.update', id, eitrecord);
+    }
+     // Clear form
       target.firstName.value = '';
       target.lastName.value = '';
       target.gender.value = '';
       target.dob.value = '';
     },
-    'click .multidelete'() {
-      Meteor.call('records.remove', this._id);
-    },
+    'change .toggle-checked'() {
+      var id = this._id;
+      if(event.target.checked) {
+        selected.push(id);
+      } else {
+        selected.splice(selected.indexOf(id), 1);
+      }
+      console.log('remove', selected);
+   },
+   'click .multidelete'() {
+     for (var i = 0; i < selected.length; i++) {
+       var ids = selected[i];
+       Meteor.call('records.remove', ids);    
+     }
+     console.log(selected)  
+   },
   });
   
